b5896a94941e977ab15e510da269c6d5d960411f
nyzs <neazenmeh@gmail.com>
2023-06-22 19:23:42 +0200

    navigate folder

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use std::path::{Path, PathBuf};
+
 use chrono::prelude::*;
-// hide console window on Windows in release
 use eframe::egui;
 
 struct Table {
@@ -8,26 +9,30 @@ struct Table {
     rows: Vec<Row>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Row {
     filename: String,
     file_type: String,
     created_at: DateTime<Utc>,
-    file_path: String,
+    file_path: PathBuf,
 }
 //
 #[derive(Debug)]
 struct Files {
     list: Vec<Row>,
+    path: PathBuf,
 }
 
 impl Files {
     fn new() -> Self {
-        Files { list: vec![] }
+        Files {
+            list: vec![],
+            path: PathBuf::new(),
+        }
     }
 
-    fn get_files(path: String) -> Files {
-        let raw = std::fs::read_dir(path).unwrap();
+    fn get_files(&mut self) {
+        let raw = std::fs::read_dir(&self.path).unwrap();
 
         let mut rows: Vec<Row> = vec![];
         for file in raw {
@@ -50,11 +55,11 @@ impl Files {
                 file_path,
             };
 
-            println!("{:?}", &row);
+            // println!("{:?}", &row);
 
             rows.push(row);
         }
-        Files { list: rows }
+        self.list = rows
     }
 }
 
@@ -69,9 +74,11 @@ fn main() -> Result<(), eframe::Error> {
         "Type".to_owned(),
         "Created At".to_owned(),
     ];
+    // let mut files = Files::get_files("/Users/OkanT/Desktop/dev/".to_owned());
+    let mut files = Files::new();
+    files.get_files();
 
-    // let mut files = std::fs::read_dir("/Users/OkanT/Desktop/dev/").unwrap();
-    let mut files = list_files("/Users/OkanT/Desktop/dev/".to_owned());
+    // println!("debug {:?}", &d);
 
     eframe::run_simple_native("pail", options, move |ctx, frame| {
         egui::CentralPanel::default().show(ctx, |ui| {
@@ -95,61 +102,59 @@ fn main() -> Result<(), eframe::Error> {
             //         }
             //     }
             // });
+            // let f = files.path.clone();
+            // if ui.button("..").clicked() {
+            //     change_list(
+            //         &mut files,
+            //         files.path.parent().unwrap().to_str().unwrap().to_owned(),
+            //     );
+            // }
             egui::Grid::new("some_unique_id")
                 .striped(true)
                 .min_col_width(180.0)
                 .show(ui, |ui| {
+                    // let list = &mut files;
+                    // let f = files.path.clone();
+                    if ui.button("..").clicked() {
+                        println!("hello guy, {:?}", &files.path);
+                        files.path = files.path.parent().unwrap().to_path_buf();
+                        // change_list(&mut files, f.parent().unwrap().to_str().unwrap());
+
+                        files.get_files();
+                    }
+
                     for col in &cols {
                         ui.label(col);
                     }
                     ui.end_row();
 
-                    for file in &files {
+                    // let list = &mut files.list;
+
+                    for file in &files.list {
                         if ui.selectable_label(false, &file.filename).clicked() {
                             // files = list_files(file.file_path);
-                            println!("{}", &file.file_path)
+                            // println!("{}", &file.file_path)
+                            files.path = file.file_path.clone();
+
+                            // change_list(&mut files, file.file_path.as_str());
                         }
                         // ui.label(&file.filename);
                         ui.label(&file.file_type);
                         ui.label(&file.created_at.to_string());
                         ui.end_row();
                     }
+                    files.get_files();
                 });
         });
     })
 }
 
-fn list_files(path: String) -> Vec<Row> {
-    let raw = std::fs::read_dir(path).unwrap();
-
-    let mut rows: Vec<Row> = vec![];
-    for file in raw {
-        let file = file.unwrap();
-
-        let filename = get_filename(&file);
-
-        let time = get_systemtime(&file);
-
-        let file_type = get_filetype(&file);
-
-        let created_at: DateTime<Utc> = time.clone().into();
-
-        let file_path = get_filepath(&file);
-
-        let row = Row {
-            filename,
-            file_type,
-            created_at,
-            file_path,
-        };
-
-        println!("{:?}", &row);
-
-        rows.push(row);
-    }
-    rows
-}
-
+// fn change_list(files: &mut Files, path: &str) {
+//     let f = Files::get_files(path.to_owned());
+//
+//     files.list = f.list;
+// }
+//
 fn get_filename(file: &std::fs::DirEntry) -> String {
     file.file_name().into_string().unwrap()
 }
@@ -165,7 +170,6 @@ fn get_filetype(file: &std::fs::DirEntry) -> String {
         false => "File".to_owned(),
     }
 }
-fn get_filepath(file: &std::fs::DirEntry) -> String {
-    let f: String = file.path().into_os_string().into_string().unwrap();
-    f
+fn get_filepath(file: &std::fs::DirEntry) -> PathBuf {
+    file.path().to_path_buf()
 }
