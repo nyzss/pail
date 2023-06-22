424b78d8356f534c605ff90453138059948106a2
nyzs <neazenmeh@gmail.com>
2023-06-22 22:25:52 +0200

    refactorng

--- src/main.rs
+++ src/main.rs
@@ -24,10 +24,10 @@ struct Files {
 }
 
 impl Files {
-    fn new() -> Self {
+    fn new(path: Option<String>) -> Self {
         Files {
             list: vec![],
-            path: PathBuf::from("/"),
+            path: PathBuf::from(path.unwrap_or("/".to_owned())),
         }
     }
 
@@ -38,15 +38,15 @@ impl Files {
         for file in raw {
             let file = file.unwrap();
 
-            let filename = get_filename(&file);
+            let filename = Files::get_filename(&file);
 
-            let time = get_systemtime(&file);
+            let time = Files::get_systemtime(&file);
 
-            let file_type = get_filetype(&file);
+            let file_type = Files::get_filetype(&file);
 
             let created_at: DateTime<Utc> = time.clone().into();
 
-            let file_path = get_filepath(&file);
+            let file_path = Files::get_filepath(&file);
 
             let row = Row {
                 filename,
@@ -55,11 +55,36 @@ impl Files {
                 file_path,
             };
 
-            // println!("{:?}", &row);
-
             rows.push(row);
         }
         self.list = rows
+    }
+    fn find_folders(&self, filter: String) {
+        for list in &self.list {
+            if list.file_type == "Folder" {
+                println!("filter: {:?}", filter);
+                println!("{:?}", list);
+            }
+        }
+    }
+
+    fn get_filename(file: &std::fs::DirEntry) -> String {
+        file.file_name().into_string().unwrap()
+    }
+    fn get_systemtime(file: &std::fs::DirEntry) -> std::time::SystemTime {
+        file.metadata().unwrap().created().unwrap()
+    }
+
+    fn get_filetype(file: &std::fs::DirEntry) -> String {
+        let f = file.file_type().unwrap();
+
+        match f.is_dir() {
+            true => "Folder".to_owned(),
+            false => "File".to_owned(),
+        }
+    }
+    fn get_filepath(file: &std::fs::DirEntry) -> PathBuf {
+        file.path().to_path_buf()
     }
 }
 
@@ -74,12 +99,14 @@ fn main() -> Result<(), eframe::Error> {
         "Type".to_owned(),
         "Created At".to_owned(),
     ];
-    // let mut files = Files::get_files("/Users/OkanT/Desktop/dev/".to_owned());
-    let mut files = Files::new();
+
+    let mut files = Files::new(Some("/Users/OkanT/Desktop/dev/".to_owned()));
     files.get_files();
 
-    let mut search: String = String::new();
-    // println!("debug {:?}", &d);
+    let mut filter_search: String = String::new();
+    let mut fuzzy_search: String = String::new();
+
+    files.find_folders("rand".to_owned());
 
     eframe::run_simple_native("pail", options, move |ctx, _| {
         egui::CentralPanel::default().show(ctx, |ui| {
@@ -104,20 +131,30 @@ fn main() -> Result<(), eframe::Error> {
             //     }
             // });
             ui.heading("pail");
+
+            ui.horizontal(|ui| {
+                ui.label("filter");
+                ui.text_edit_singleline(&mut filter_search);
+                ui.label(&filter_search);
+            });
+            ui.label("|");
+            ui.horizontal(|ui| {
+                ui.label("fuzzy search");
+                ui.text_edit_singleline(&mut fuzzy_search);
+                ui.label(&fuzzy_search);
+            });
+
             egui::ScrollArea::vertical().show(ui, |ui| {
                 egui::Grid::new("folder_table")
                     .striped(true)
                     .min_col_width(320.0)
                     .show(ui, |ui| {
-                        ui.label("filter");
-                        ui.text_edit_singleline(&mut search);
-                        ui.label(&search);
                         if files.path != PathBuf::from("/") {
                             if ui.button("..").clicked() {
                                 println!("hello guy, {:?}", &files.path);
                                 // println!("strip prefix: {:?}", &files.path.strip_prefix());
                                 files.path = files.path.parent().unwrap().to_path_buf();
-                                search = String::new();
+                                filter_search = String::new();
 
                                 // change_list(&mut files, f.parent().unwrap().to_str().unwrap());
 
@@ -136,8 +173,8 @@ fn main() -> Result<(), eframe::Error> {
                             if file
                                 .filename
                                 .to_lowercase()
-                                .contains(&search.to_lowercase())
-                                || &search == ""
+                                .contains(&filter_search.to_lowercase())
+                                || &filter_search == ""
                             {
                                 if ui.selectable_label(false, &file.filename).clicked() {
                                     if &file.file_type == "Folder" {
@@ -145,7 +182,7 @@ fn main() -> Result<(), eframe::Error> {
                                     }
                                     println!("clicked on: {:?}", &file.file_path);
 
-                                    search = String::new();
+                                    filter_search = String::new();
 
                                     // change_list(&mut files, file.file_path.as_str());
                                 }
@@ -160,23 +197,4 @@ fn main() -> Result<(), eframe::Error> {
             });
         });
     })
-}
-
-fn get_filename(file: &std::fs::DirEntry) -> String {
-    file.file_name().into_string().unwrap()
-}
-fn get_systemtime(file: &std::fs::DirEntry) -> std::time::SystemTime {
-    file.metadata().unwrap().created().unwrap()
-}
-
-fn get_filetype(file: &std::fs::DirEntry) -> String {
-    let f = file.file_type().unwrap();
-
-    match f.is_dir() {
-        true => "Folder".to_owned(),
-        false => "File".to_owned(),
-    }
-}
-fn get_filepath(file: &std::fs::DirEntry) -> PathBuf {
-    file.path().to_path_buf()
 }
